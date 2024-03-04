8563eb265ff2eefd29af9b90a99a69431b2e911c
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-03-04 14:56:36 -0800

    Finished Carousel Implementation, action buttons left to do

--- client/src/components/Related_Items/Card.jsx
+++ client/src/components/Related_Items/Card.jsx
@@ -2,24 +2,41 @@ import React from 'react';
 import {useState} from 'react';
 
 const Card = function ({item, setID}) {
-  const [currentCard, setCurrentCard] = useState(item.id);
-  console.log(currentCard);
-  console.log(item);
+  const [currentCard, setCurrentCard] = useState(item.product.id);
+
+  let img_url = item.styles.results[0].photos[0].thumbnail_url || `https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg`;
+  let preStarRatings = item.meta.ratings;
+  let product = item.product;
 
   const changeID = function () {
     setID(currentCard);
   }
 
+  const setRating = function() {
+    let reviewNumber = 0;
+    let reviewWeight = 0;
+    for (var key in preStarRatings) {
+      reviewNumber += Number(preStarRatings[key]);
+      reviewWeight += (preStarRatings[key] * key);
+    }
+    return reviewWeight / reviewNumber;
+  }
+
+  let star = setRating()
+
   return (
-    <div onClick={changeID}>
-      <button>ACTION BUTTON</button>
-      <p>img</p>
-      <div>{item.category}</div>
-      <div>{item.name}</div>
-      <span>{item.default_price}</span>
-      <div>STAR RATING</div>
+    <div onClick={changeID} className="r-i-card">
+      <button>Action Button</button>
+      <img className="r-i-img" src={img_url}></img>
+      <div>{product.category}</div>
+      <div>{product.name}</div>
+      <div>{product.default_price}</div>
+      <div  className="Stars" style={{ '--rating': star }}></div>
     </div>
   )
 };
 
 export default Card;
+
+//https://www.needpix.com/photo/1113016/
+//https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg
