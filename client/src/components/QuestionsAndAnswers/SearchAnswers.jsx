9c3e5e49902c81f9e4f45b215ebd327cd5f27296
Richard Li <richardli1745@gmail.com>
2024-03-04 16:49:56 -0800

    added Search,Q,A, small detail functionality

---
+++ client/src/components/QuestionsAndAnswers/SearchAnswers.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const SearchAnswers = ({ handleSearch }) => {
+
+  const [search, setSearch] = useState('');
+
+  const handleSearchInput = (e) => {
+    setSearch(e.target.value);
+    if(search.length >= 3) {
+      handleSearch(search);
+    }
+  }
+
+  return (
+    <div className="search-container">
+    <input className="search-bar" placeholder="Have a question? Search for answers…" value={search} onChange={handleSearchInput}/>
+    </div>
+
+  )
+
+}
+
+export default SearchAnswers;
