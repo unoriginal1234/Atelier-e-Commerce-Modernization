322b378d8b57aa18cf232b5111166c7f1cc7dd80
unoriginal1234 <eric.t.roth@gmail.com>
2024-03-05 15:40:48 -0800

    Added a test file for Ratings and Review component

---
+++ client/src/components/RatingsAndReviews/ratings.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react'
+import {render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import RatingsAndReviews from './RatingsAndReviews.jsx'
+
+test('renders title', () => {
+  const {asFragment, getByText} = render(<RatingsAndReviews />)
+  expect(getByText('Ratings and Reviews')).toBeInTheDocument()
+})
