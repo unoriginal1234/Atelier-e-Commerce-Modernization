f0c9829e7d56350c5d86653755c2aa7fc02f19e2
unoriginal1234 <eric.t.roth@gmail.com>
2024-03-05 16:34:12 -0800

    Added new test

--- client/src/components/RatingsAndReviews/ratings.test.js
+++ client/src/components/RatingsAndReviews/ratings.test.js
@@ -6,9 +6,14 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import RatingsAndReviews from './RatingsAndReviews.jsx'
+import ReviewBreakdown from './ReviewBreakdown.jsx'
 
 test('renders title', () => {
   const {asFragment, getByText} = render(<RatingsAndReviews />)
   expect(getByText('Ratings and Reviews')).toBeInTheDocument()
 })
 
+test('renders subtitle', () => {
+  const {asFragment, getByText} = render(<RatingsAndReviews />)
+  expect(getByText('Rating Breakdown')).toBeInTheDocument()
+})
