69d4f39c64a0fabd3e047180cc3e380145421523
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-03-11 16:03:36 -0700

    added style changes for secret comparison to have an arrow and look more presentable

--- client/src/components/Related_Items/Comparison.jsx
+++ client/src/components/Related_Items/Comparison.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
+import { TiArrowSortedDown } from "react-icons/ti";
 import RelatedCategory from './RelatedCategory.jsx';
 
 const Comparison = function ({ bothCategories }) {
 
   return (
-    <div className="r-i-secret">
-      <div className="r-i-secret-title">Compare</div>
-      {Object.keys(bothCategories).map((key, index) => {
-        return <RelatedCategory category={key} key={index} values={bothCategories}/>
-      })}
+    <div className="r-i-secret-holder">
+      <div className="r-i-secret">
+        <div className="r-i-secret-title">Compare</div>
+        {Object.keys(bothCategories).map((key, index) => {
+          return <RelatedCategory category={key} key={index} values={bothCategories}/>
+        })}
+        </div>
+        <TiArrowSortedDown className="r-i-secret-arrow"/>
     </div>
   )
 };
