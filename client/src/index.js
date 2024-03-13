d31e4b1bbb995ec9671aae8b01ee329e9cced6a2
Raul Garcia <raulgarciain@gmail.com>
2024-03-13 14:28:40 -0700

    Dark mode

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@
 //client/index.js
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
+import { DarkModeProvider } from './DarkModeContext';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render( <DarkModeProvider><App /></DarkModeProvider>);
+
